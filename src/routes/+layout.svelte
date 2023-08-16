7af6492eaf4ebd81631a6a5ff29f4684b0da02b7
code406 <davidpm99@gmail.com>
2023-08-16 19:25:06 +0200

    added pico, tailwind and lucide. simple nav, main container

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import "../app.css";
+    import '@picocss/pico';
+    import '@picocss/pico/css/pico.colors.min.css';
+    import { Github, Globe, Moon } from 'lucide-svelte';
+
+    const title = 'Guess The Year';
+    const subtitle = 'Game';
+</script>
+
+<svelte:head>
+    <title>{title} {subtitle}</title>
+</svelte:head>
+
+<main class="container">
+    <nav>
+        <ul class="flex flex-col items-end">
+            <li class="pb-0 lowercase font-bold text-2xl">{title}</li>
+            <li class="pt-0 uppercase font-bold pico-color-amber-300 text-xs">{subtitle}</li>
+        </ul>
+        <ul>
+            <li><a class="secondary" href="/"><Globe /></a></li>
+            <li><a class="secondary" href="/"><Github /></a></li>
+            <li><a class="secondary" href="/"><Moon /></a></li>
+        </ul>
+    </nav>
+
+    <slot />
+</main>
+
+<style>
+    nav {
+        padding-block: 1rem;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="text-center">
+    <h2>Some text</h2>
+    <p>Lorem ipsum dolor sit amet consecteur adispiscing elit. Lorem ipsum dolor sit amet consecteur adispiscing elit. Lorem ipsum dolor sit amet consecteur adispiscing elit. Lorem ipsum dolor sit amet consecteur adispiscing elit. Lorem ipsum dolor sit amet consecteur adispiscing elit.</p>
+</div>
