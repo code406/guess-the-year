ad1ba47e372d2efed248db98d1f91dabb04a6cfa
code406 <davidpm99@gmail.com>
2023-08-17 14:00:24 +0200

    centered error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+  
+<div class="px-6 py-12 h-full mx-auto flex flex-col items-center justify-center text-center">
+    <h1 class="text-3xl pb-3">{$page.status}</h1>
+    <h2 class="text-lg">{$page.error?.message}</h2>
+</div>
