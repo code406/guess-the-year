582f70feb9ce71796d4ece446bdc20869b805b99
code406 <davidpm99@gmail.com>
2023-08-17 13:59:42 +0200

    fonts: Rubik and JetBrains Mono

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      sans: ['Rubik', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      mono: ['JetBrains Mono', 'ui-monospace', 'monospace']
+    },
     extend: {},
   },
   plugins: [],
