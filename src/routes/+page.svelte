c2ca56b4ddef196f60cc6f256791f56d2649edd2
code406 <davidpm99@gmail.com>
2023-08-18 02:36:21 +0200

    dynamic year in landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <article class="text-center flex flex-col justify-center items-center [text-wrap:balance] py-10 px-8">
     <p class="text-xl w-11/12 3xs:w-3/4 2xs:w-3/5 xs:w-full">Guess the year when the event happened.</p>
-    <p class="text-xl w-10/12 3xs:w-4/6 2xs:w-7/12 xs:w-full mt-4 xs:mt-0">Answers range from 1900 to 2021.</p>
+    <p class="text-xl w-10/12 3xs:w-4/6 2xs:w-7/12 xs:w-full mt-4 xs:mt-0">Answers range from 1900 to {new Date().getFullYear()}.</p>
     <p class="mt-6 text-[var(--pico-muted-color)]">Playing Time: 1-2 minutes.</p>
     <a href="/game" role="button" class="text-lg mt-8 py-2 font-bold">start game</a>
 </article>
