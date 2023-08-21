07fcf013b701526f113f040178a1c079e8116e5b
code406 <davidpm99@gmail.com>
2023-08-21 18:11:22 +0200

    scale transition for game, fly transition for home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,61 @@
 <script>
     import Marquee from '$lib/Marquee.svelte';
     import { startYear, endYear } from '$lib/Config.svelte';
+    import { fly } from 'svelte/transition';
 </script>
 
-<article class="px-0 py-2 mb-5 select-none"><Marquee /></article>
+<div in:fly>
+    <article class="px-0 py-2 mb-5 select-none"><Marquee /></article>
 
-<article class="main-article text-center flex flex-col justify-center items-center py-12 2xs:py-16 mb-6">
-    <p class="text-2xl font-bold">
-        <span class="block">Guess the year when</span>
-        <span class="block">the event happened.</span>
-    </p>
-    <p class="text-sm mt-6 text-[var(--pico-muted-color)]">
-        <span class="inline-block">Answers range</span>
-        <span class="inline-block">from {startYear} to {endYear}.</span>
-    </p>
-    <p class="text-sm text-[var(--pico-muted-color)]">Playing time: 1-2 minutes.</p>
-    <a href="/game" role="button" class="text-lg w-40 sm:text-base mt-8 py-2 font-bold">start game</a>
-</article>
+    <article class="main-article text-center flex flex-col justify-center items-center py-12 2xs:py-16 mb-6">
+        <p class="text-2xl font-bold">
+            <span class="block">Guess the year when</span>
+            <span class="block">the event happened.</span>
+        </p>
+        <p class="text-sm mt-6 text-[var(--pico-muted-color)]">
+            <span class="inline-block">Answers range</span>
+            <span class="inline-block">from {startYear} to {endYear}.</span>
+        </p>
+        <p class="text-sm text-[var(--pico-muted-color)]">Playing time: 1-2 minutes.</p>
+        <a href="/game" role="button" class="text-lg w-40 sm:text-base mt-8 py-2 font-bold">start game</a>
+    </article>
 
-<article class="px-0 py-2 mb-5 select-none"><Marquee /></article>
+    <article class="px-0 py-2 mb-5 select-none"><Marquee /></article>
 
-<article class="p-8 sm:py-10 sm:px-12">
-    <h2 class="font-sans text-xl py-1 font-bold opacity-90">About this project</h2>
-    <p class="pt-6 text-[var(--pico-muted-color)]">
-        This project is a clone of
-        <a href="https://davjhan.com/" role="link" class="secondary">David Han</a>'s "Guess the Year game"
-        that I'm building for fun, to learn and use various technologies. All the code is available on 
-        <a href="https://github.com/code406/guess-the-year" role="link" class="secondary">Github</a>,
-        so feel free to check it out!
-    </p>
-    <p class="pt-5 text-[var(--pico-muted-color)]">
-        Anyway, here's my experience so far:
-    </p>
-    <ul class="pl-8 pt-5 list-disc list-inside text-[var(--pico-muted-color)] space-y-5">
-        <li>
-            <a href="https://kit.svelte.dev" role="link" class="secondary">SvelteKit</a> is a joy to
-            work with. I'm still learning, but I'm already a big fan and I hope the community keeps growing.
-        </li>
-        <li>
-            I like how <a href="https://v2.picocss.com/docs" role="link" class="secondary">Pico CSS</a>
-            keeps it minimal and takes care of styling for me, and I also like how quickly I can tweak
-            things with <a href="https://tailwindcss.com" role="link" class="secondary">Tailwind CSS</a>.
-            They do conflict with each other, but it's nice to have the best of both worlds.
-        </li>
-        <li>
-            <a href="https://www.typescriptlang.org" role="link" class="secondary">Typescript</a> is not my
-            favorite language, but it's definitely an improvement over vanilla JavaScript.
-        </li>
-        <li>
-            Deployment to <a href="https://vercel.com" role="link" class="secondary">Vercel</a>
-            took about 5 minutes, integration with Github and SvelteKit is pretty impressive.
-        </li>
-    </ul>
-    <p class="pt-5 text-[var(--pico-muted-color)]">
-        All in all, developer experience has been great!
-    </p>
-</article>
+    <article class="p-8 sm:py-10 sm:px-12">
+        <h2 class="font-sans text-xl py-1 font-bold opacity-90">About this project</h2>
+        <p class="pt-6 text-[var(--pico-muted-color)]">
+            This project is a clone of
+            <a href="https://davjhan.com/" role="link" class="secondary">David Han</a>'s "Guess the Year game"
+            that I'm building for fun, to learn and use various technologies. All the code is available on 
+            <a href="https://github.com/code406/guess-the-year" role="link" class="secondary">Github</a>,
+            so feel free to check it out!
+        </p>
+        <p class="pt-5 text-[var(--pico-muted-color)]">
+            Anyway, here's my experience so far:
+        </p>
+        <ul class="pl-8 pt-5 list-disc list-inside text-[var(--pico-muted-color)] space-y-5">
+            <li>
+                <a href="https://kit.svelte.dev" role="link" class="secondary">SvelteKit</a> is a joy to
+                work with. I'm still learning, but I'm already a big fan and I hope the community keeps growing.
+            </li>
+            <li>
+                I like how <a href="https://v2.picocss.com/docs" role="link" class="secondary">Pico CSS</a>
+                keeps it minimal and takes care of styling for me, and I also like how quickly I can tweak
+                things with <a href="https://tailwindcss.com" role="link" class="secondary">Tailwind CSS</a>.
+                They do conflict with each other, but it's nice to have the best of both worlds.
+            </li>
+            <li>
+                <a href="https://www.typescriptlang.org" role="link" class="secondary">Typescript</a> is not my
+                favorite language, but it's definitely an improvement over vanilla JavaScript.
+            </li>
+            <li>
+                Deployment to <a href="https://vercel.com" role="link" class="secondary">Vercel</a>
+                took about 5 minutes, integration with Github and SvelteKit is pretty impressive.
+            </li>
+        </ul>
+        <p class="pt-5 text-[var(--pico-muted-color)]">
+            All in all, developer experience has been great!
+        </p>
+    </article>
+</div>
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import allQuestions from '$lib/questions.json';
     import { startYear, endYear } from '$lib/Config.svelte';
+    import { scale } from 'svelte/transition';
 
     const initialGuess = Math.round((startYear + endYear) / 2);
     let guess = initialGuess;
@@ -28,53 +29,55 @@
     }
 </script>
 
-<!-- score and lives -->
-<div class="flex justify-between">
-    <article class="flex py-2 space-x-4 mb-5">
-        <p>score</p>
-        <strong class="text-[var(--pico-primary)]">{round}</strong>
-    </article>
-    <article class="flex py-2 space-x-4 mb-5">
-        <p>lives</p>
-        <strong class="text-[var(--pico-ok)]">{lives}</strong>
+<div in:scale={{ duration: 500, delay: -200 }}>
+    <!-- score and lives -->
+    <div class="flex justify-between">
+        <article class="flex py-2 space-x-4 mb-5">
+            <p>score</p>
+            <strong class="text-[var(--pico-primary)]">{round}</strong>
+        </article>
+        <article class="flex py-2 space-x-4 mb-5">
+            <p>lives</p>
+            <strong class="text-[var(--pico-ok)]">{lives}</strong>
+        </article>
+    </div>
+
+    <!-- question area -->
+    <article class="py-8 sm:py-10 question-area flex flex-col justify-center text-center items-center [text-wrap:balance]">
+        <!-- question -->
+        <p class="text-[var(--pico-muted-color)] pb-4">
+            <span class="inline-block">What year did the following</span>
+            <span class="inline-block">event happen?</span>
+        </p>
+        <p class="text-xl xs:text-2xl font-bold pb-8 px-1 h-32 4xs:h-28 xs:h-24 flex items-center">
+            <span>{@html question.question}</span>
+        </p>
+
+        <!-- slider value -->
+        <div class="w-28 3xs:mb-2">
+            <label for="guess-number">Guess:</label>
+            <input type="number" name="guess-number" min={startYear} max={endYear}
+                step=1 bind:value={guess} on:blur={fixGuess}
+                class="text-xl text-center font-bold border-[var(--pico-primary-background)] border-2 rounded-md">
+        </div>
+
+        <!-- slider -->
+        <div class="px-3 sm:px-6 flex w-full space-x-4 py-2">
+            <span>{startYear}</span>
+            <input type="range" name="guess-slider" min={startYear} max={endYear}
+                step=1 bind:value={guess} class="pt-1">
+            <span>{endYear}</span>
+        </div>
+        <label for="guess-slider" class="mb-8 text-sm text-[var(--pico-muted-color)]">
+            drag the slider or type the answer
+        </label>
+
+        <!-- confirm button -->
+        <button role="button" class="text-lg sm:text-base mb-1 py-2 px-4 w-2/5 font-bold" on:click={nextRound}>
+            confirm
+        </button>
     </article>
 </div>
-
-<!-- question area -->
-<article class="py-8 sm:py-10 question-area flex flex-col justify-center text-center items-center [text-wrap:balance]">
-    <!-- question -->
-    <p class="text-[var(--pico-muted-color)] pb-4">
-        <span class="inline-block">What year did the following</span>
-        <span class="inline-block">event happen?</span>
-    </p>
-    <p class="text-xl xs:text-2xl font-bold pb-8 px-1 h-32 4xs:h-28 xs:h-24 flex items-center">
-        <span>{@html question.question}</span>
-    </p>
-
-    <!-- slider value -->
-    <div class="w-28 3xs:mb-2">
-        <label for="guess-number">Guess:</label>
-        <input type="number" name="guess-number" min={startYear} max={endYear}
-            step=1 bind:value={guess} on:blur={fixGuess}
-            class="text-xl text-center font-bold border-[var(--pico-primary-background)] border-2 rounded-md">
-    </div>
-
-    <!-- slider -->
-    <div class="px-3 sm:px-6 flex w-full space-x-4 py-2">
-        <span>{startYear}</span>
-        <input type="range" name="guess-slider" min={startYear} max={endYear}
-            step=1 bind:value={guess} class="pt-1">
-        <span>{endYear}</span>
-    </div>
-    <label for="guess-slider" class="mb-8 text-sm text-[var(--pico-muted-color)]">
-        drag the slider or type the answer
-    </label>
-
-    <!-- confirm button -->
-    <button role="button" class="text-lg sm:text-base mb-1 py-2 px-4 w-2/5 font-bold" on:click={nextRound}>
-        confirm
-    </button>
-</article>
 
 <style>
     input[type=number]::-webkit-outer-spin-button,
