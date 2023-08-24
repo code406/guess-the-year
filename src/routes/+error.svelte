ea02c91426d15ecba3117d1c31798a0323e49e26
code406 <davidpm99@gmail.com>
2023-08-25 01:19:44 +0200

    small changes to error page text-color and alignment

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
     import { page } from '$app/stores';
 </script>
   
-<div class="px-6 py-12 h-full mx-auto flex flex-col items-center justify-center text-center">
-    <h1 class="font-sans text-3xl pb-3">{$page.status}</h1>
-    <h2 class="font-sans text-lg">{$page.error?.message}</h2>
+<div class="flex-grow px-6 pt-12 pb-36 flex flex-col justify-center text-center">
+    <h1 class="font-sans text-[var(--pico-muted-color)] text-3xl pb-3">{$page.status}</h1>
+    <h2 class="font-sans text-[var(--pico-muted-color)] text-lg">{$page.error?.message}</h2>
 </div>
